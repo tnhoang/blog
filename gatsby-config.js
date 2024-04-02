1f0c5955bea96401a3837e7999da4bf60b762a0e
tnhoang <tnhoang088@gmail.com>
2024-04-03 01:44:25 +0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,9 +81,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `tnhoang`,
+        short_name: `tnhoang-blog`,
+        description: `Personal blog`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
